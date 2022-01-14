14a06b9035b3451a787e9461f9815aca35f36dea
Nastya <n_m_666@mail.ru>
2022-01-14 13:49:06 +0500

    замена шаблона

--- src/scripts/data.js
+++ src/scripts/data.js
@@ -2,7 +2,7 @@ const dom = document.querySelector('.js-body')
 
 let currentData = new Date();
 const month = currentData.getMonth() + 1
-let data = currentData.getDate() + '' + month + '21'
+let data = currentData.getDate() + '' + month + '22'
 
 
 dom.innerHTML = dom.innerHTML.replace(/curData/gi, data)
